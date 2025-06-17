8eef85614db8ec8fa52893a0f6bd8558f0b7cbb7
CristianEGonzalez <cristianarg_87@hotmail.com>
2025-06-17 10:21:23 -0300

    Limitación de publicaciones recientes a una cantidad específica

--- src/components/PublicacionesRecientes/PublicacionesRecientes.jsx
+++ src/components/PublicacionesRecientes/PublicacionesRecientes.jsx
@@ -2,29 +2,48 @@ import { useEffect, useState } from 'react';
 import { Publicacion } from "../Publicacion/Publicacion";
 import { getPublicaciones /*, getPublicacionesById*/ } from '../../services/postApi.js';
 
+const obtenerPublicacionesRecientes = (publicaciones, cantidad) => {
+  if (!publicaciones || publicaciones.length === 0) {
+    return [];
+  }
+  const publicacionesOrdenadas = [...publicaciones].sort((a, b) => {
+    const datoA = new Date(a.updatedAt);
+    const datoB = new Date(b.updatedAt);
+
+    // Ordenar de forma descendente (más reciente primero)
+    return datoB.getTime() - datoA.getTime();
+  });
+  return publicacionesOrdenadas.slice(0, cantidad);
+};
 
 function PublicacionesRecientes() {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [cargando, setCargando] = useState(true); //Evaluar si lo dejamos o lo quitamos y también se borraría el finally
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchPublicaciones = async () => {
       try {
-        const publicaciones = await getPublicaciones(); 
-        setPosts(publicaciones);
+        setCargando(true);
+        const publicaciones = await getPublicaciones();
+        
+        // --- Definimos cuántas publicaciones recientes se mostrarán en el home ---
+        const publicacionesRecientes = obtenerPublicacionesRecientes(publicaciones, 8);
+        setPosts(publicacionesRecientes);
+        // ---------------------------------
+
       } catch (err) {
         setError("Error al cargar las publicaciones: " + (err.message || 'Por favor, intenta de nuevo más tarde.'));
       } finally {
-        setLoading(false);
+        setCargando(false);
       }
     };
 
     fetchPublicaciones();
   }, []); // El array vacío asegura que esta función se ejecuta solo una vez al montar el componente
 
-  if (loading) {
-    return <div className="text-center my-4">Cargando publicaciones...</div>;
+  if (cargando) {
+    return <div className="text-center my-4">Cargando publicaciones...</div>; //Se podría mejorar la estética del cargando
   }
 
   if (error) {
